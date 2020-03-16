07053e7f817c49436ae95caa101887ee28857bc2
Ahmed Moustafa <amoustafa@gmail.com>
2020-03-17 01:27:04 +0200

    Create prerequisites.sh

---
+++ prerequisites.sh
@@ -0,0 +1,31 @@
+apt-get -y install apt-utils \
+dialog \
+software-properties-common \
+vim nano emacs \
+rsync curl wget \
+build-essential libtool autotools-dev automake autoconf cmake \
+libboost-dev libboost-all-dev libboost-system-dev libboost-program-options-dev libboost-iostreams-dev libboost-filesystem-dev \
+gfortran libgfortran3 \
+python python-pip python-dev python3.7 python3.7-dev python3-pip python3-venv \
+default-jre default-jdk ant \
+screen htop parallel \
+gnupg \
+lsof \
+git \
+locate \
+unrar \
+bc \
+aptitude \
+libssl-dev libcurl4-openssl-dev \
+libxml2-dev \
+libmagic-dev \
+hdf5-* libhdf5-* \
+fuse libfuse-dev \
+libtbb-dev \
+unzip liblzma-dev libbz2-dev \
+bison libbison-dev \
+flex \
+libgmp3-dev \
+libncurses5-dev libncursesw5-dev \
+liblzma-dev \
+caffe-cpu
